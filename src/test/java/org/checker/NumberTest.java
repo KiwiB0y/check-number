3a939198bd429c1bbc2acb67446d2466468acb2a
KiwiB0y <caki6ko@yahoo.com>
2022-06-25 19:57:56 +0200

    Add invalid test for class Number

--- src/test/java/org/checker/NumberTest.java
+++ src/test/java/org/checker/NumberTest.java
@@ -3,6 +3,7 @@ package org.checker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class NumberTest {
@@ -11,5 +12,11 @@ class NumberTest {
     void testValidUniversityNumber() {
         Number universityNumber = new Number(1, 9, 4, 2, 0, 0, 2);
         assertTrue(universityNumber.isValid());
+    }
+    @Test
+    @DisplayName("Invalid University number fails")
+    void testInvalidUniversityNumber() {
+        Number universityNumber = new Number(1, 9, 4, 2, 0, 0, 0);
+        assertFalse(universityNumber.isValid());
     }
 }
